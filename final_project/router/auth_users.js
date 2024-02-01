ed8aee31a8276cdbbb846ec3d12edf70b7e9355c
maina-duseja <mainaduseja17@gmail.com>
2024-02-01 06:46:01 -0500

    changes made from theia lab environment

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,27 +3,97 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{"username":"mini","password":"myFin@lL@b"}];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+const userMatches = users.filter((user) => user.username === username);
+    return userMatches.length > 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
+
+regd_users.post("/customer/login", function auth(req,res,next){
+   if(req.session.authorization) {
+       token = req.session.authorization['accessToken'];
+       jwt.verify(token, "access",(err,user)=>{
+           if(!err){
+               req.user = user;
+               next();
+           }
+           else{
+               return res.status(403).json({message: "User not authenticated"})
+           }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
+});
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  console.log("login: ", req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+            accessToken,username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const username = req.session.authorization['username'];
+  const review = req.body.review;
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  else {
+    book["reviews"][username] = review;
+    return res.status(200).json({ message: "Review added successfully" });
+  }
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    let book_reviewer = req.session.authorization['username'];
+    let filtered_review = books[isbn]["reviews"];
+    if (filtered_review[book_reviewer]){
+        delete filtered_review[book_reviewer];
+        res.send(`Reviews for the book with ISBN  ${isbn} posted by the user ${book_reviewer} is deleted.`);
+    }
+    else{
+        res.send("Unable to delete the review, as it was been posted by a different user");
+    }
+  });   
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
